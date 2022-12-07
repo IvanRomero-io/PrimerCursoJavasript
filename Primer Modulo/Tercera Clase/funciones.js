dac91ceaa4e4b7a5226966d90897d592140fa3a4
IvanRomero-io <ivandejesusrl2001@gmail.com>
2022-12-07 09:27:19 -0500

    final clase funciones

--- Primer Modulo/Tercera Clase/funciones.js
+++ Primer Modulo/Tercera Clase/funciones.js
@@ -30,3 +30,13 @@ var miFuncionE = function(a, b){
 }
 
 miFuncionE();
+
+
+//una difencia en las funciones declarativas y expresivas, es que las declarativas pueden ser llamadas antes de ser
+//declaradas abajo, a esto se le llama hoisting.
+
+function solution(valor1, valor2, valor3){
+    console.log(typeof valor1)
+    console.log(typeof valor2)
+    console.log(typeof valor3)
+}
