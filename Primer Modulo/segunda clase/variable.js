d011bea4e871404f089546686e05d69e6be7e4a7
IvanRomero-io <ivandejesusrl2001@gmail.com>
2022-12-07 01:10:09 -0500

    clase 2

---
+++ Primer Modulo/segunda clase/variable.js
@@ -0,0 +1,16 @@
+var nombre = "ivan";
+
+var edad
+
+edad = 21;
+
+var caracteristica = edad + nombre
+
+alert(caracteristica)
+
+var elementos  = ["Computadora", "Celular"]
+
+var persona = {
+    nombre: "diego",
+    edad: 30
+}
