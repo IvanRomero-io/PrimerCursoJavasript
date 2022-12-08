ea19ac7e1c80e71fa92b65f12a2fcfbc7da76320
IvanRomero-io <ivandejesusrl2001@gmail.com>
2022-12-08 01:14:58 -0500

    clase 5 hoisting

---
+++ Primer Modulo/Quinta Clase/hoisting.js
@@ -0,0 +1,27 @@
+var miNombre = undefined
+
+console.log(miNombre)
+
+miNombre = "diego";
+
+//EN ESTE EJEMPLO, EL MESAJE QUE SE VA A ENSEÑAR EN LA CONSOLA
+//ES UN UNDEFINED, PORQUE LA ACCION SE HIZO ANTES DE DARLE UN VALOR A LA VARIABLE
+
+//Ejemplo con funciones
+
+console.log(tuNombre)
+
+
+
+
+function hey(){
+    console.log("hola " + tuNombre)
+}
+
+
+
+
+hey();
+
+
+var tuNombre = "manuel"
