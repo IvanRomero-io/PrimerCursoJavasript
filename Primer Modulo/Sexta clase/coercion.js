b81a1cd3b8cde892c1c04415686bd280f46b7195
IvanRomero-io <ivandejesusrl2001@gmail.com>
2022-12-08 01:38:00 -0500

    clase 6, coersion y cambios de valor de variables

---
+++ Primer Modulo/Sexta clase/coercion.js
@@ -0,0 +1,21 @@
+var a = 4 + "7";
+console.log(a);
+
+var b= 4 * "7";
+
+console.log(b);
+
+//cuando se multiplica, la computadora ya sabe que es una operacion matematica
+
+var c = 20
+var d = c + ""
+
+console.log(b);
+
+var e = String(c);
+
+console.log(c + " esta funcionando este")
+
+var f = Number(e);
+
+// El uso de variables y convertirlas a otro valor pero cambiando su coercion (numeros, string), de otra variable.
