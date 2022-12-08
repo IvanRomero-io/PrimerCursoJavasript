846bf075f09d085d976a684ae5423b1925da27ce
IvanRomero-io <ivandejesusrl2001@gmail.com>
2022-12-08 01:49:23 -0500

    clase 7, truthy yb falsy

---
+++ Primer Modulo/Septima Clase/thurthy.js
@@ -0,0 +1,21 @@
+Boolean(0)
+
+//false
+
+Boolean(null)
+
+//falso
+
+Boolean(Nan)
+
+//falso
+
+Boolean(undefined)
+
+//falso
+
+Boolean(1);
+
+//true
+
+//si tiene un caracter, icono, numero, funciones o letra es sufuiente para que sea true.
