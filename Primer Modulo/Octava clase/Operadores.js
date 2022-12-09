1edfc3db2b87a3a726ffa953ab47d6a97e9a3d04
IvanRomero-io <ivandejesusrl2001@gmail.com>
2022-12-09 09:02:24 -0500

    operadores

---
+++ Primer Modulo/Octava clase/Operadores.js
@@ -0,0 +1,38 @@
+3 + 2 //suma
+50 - 10 //resta
+10 * 10 //multiplicacion
+20/ 2  //division
+
+
+"Ivan " + "Romero" //Suma los string y los vuelve 1
+
+true
+!true
+
+false
+!false
+
+var a = 1; //signo igual solo, le añade un valor a algo
+
+3 == "3" //Doble signo de igual, compara si son iguales, solo se fija en el valor, no en el tipo de elemento (Numero y string)
+
+3 === "3" //Triple signo de igual, compara si son PERFECTAMENTE igual, siendo el mismo valor y el mismo tipo de elemento
+
+5 < 3 //false
+
+5 > 3  //true
+
+5 <= 6 //true
+
+5 >= 6 //false
+
+6 >= 6 //true
+
+a && b //tienen que cumplirce ambas
+
+a || b //por lo menos tiene que cumplirce una
+
+var edad = 40
+edad++ //le suma 1 a la variable
+
+edad += 2 //le suma 2 a la variable
