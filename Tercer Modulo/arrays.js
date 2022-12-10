f7a02b30c9a828f0a4d5e9f6781000bfe94b4d4f
IvanRomero-io <ivandejesusrl2001@gmail.com>
2022-12-10 13:07:04 -0500

    arrays

---
+++ Tercer Modulo/arrays.js
@@ -0,0 +1,13 @@
+//crea un array con diferentes elementos
+var frutas = ["Manzana", "Platano" ,"cereza", "fresa" ]
+//añade elementos al dicho array
+var masFrutas = frutas.push("piñas")
+var otraFruta = frutas.push("uvas")
+//saca solamente el ultimo elemento
+var otraFruta = frutas.pop("uvas")
+//añade un elemento y lo pone de primero
+var nuevaLongitud = frutas.unshift("peras")
+//ELIMINA EL PRIMER ELEMENTO
+var borrarFruta = frutas.shift("peras")
+//SABER LA POSICION DE UN ELEMENTO
+var posicion = frutas.indexOf("cereza");
